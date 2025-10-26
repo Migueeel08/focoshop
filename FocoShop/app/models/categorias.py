c7676b842b1e92824618eabf4049fcc9476ca44d
Miguel Yahir Valenzuela Lerma <117939683+Migueeel08@users.noreply.github.com>
2025-10-26 01:25:26 -0700

    adios bro

--- FocoShop/app/models/categorias.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Categoria(Base):
-    __tablename__ = "categorias"
-
-    id_categoria = Column("id_categoria", Integer, primary_key=True, index=True)
-    nombre = Column("nombre", String(100), nullable=False)
